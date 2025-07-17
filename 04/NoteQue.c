9b30d4f2c490c4949be3c53f10ca1244025468d9
Isha Ray <isharay009@gmail.com>
2025-07-18 00:05:13 +0530

    Operators

---
+++ 04/NoteQue.c
@@ -0,0 +1,58 @@
+/* What are Operators in C?
+Operators are symbols that perform operations on variables and values.
+For example: +, -, *, /, = are all operators. */
+
+/* Types of Operators in C ----
+
+Type	                        Description	                            Example
+
+1. Arithmetic Operators	        Mathematical calculations	            +, -, *, /, %
+2. Relational Operators	        Comparison between values	            ==, !=, >, <, >=, <=
+3. Logical Operators	        Logical conditions	                    && (AND), `
+4. Assignment Operators	        Assign values	                        =, +=, -=, *=, /=, %=
+5. Increment/Decrement	        Increase/decrease by 1	                ++, --
+6. Bitwise Operators	        Bit-level operations	                &, `
+7. Conditional (Ternary)	    Shortcut for if-else	                (condition) ? true : false
+8. Comma Operator	            Evaluates multiple expressions	         a = (b=5, b+10);
+9. Sizeof Operator	            Returns size of a data type	             sizeof(int)
+
+*/
+
+/* Types of Instructions in C -----
+
+Type	                            Meaning	                                        Example
+1. Type Declaration             Instruction	Declares variables	                int a, b;
+2. Arithmetic Instruction	    Mathematical operations	                        c = a + b;
+3. Control Instruction	        Controls flow of program	                    if, else, while, for, etc.
+
+*/
+
+/* Type Conversion -----
+Type conversion means changing one data type to another.
+
+Implicit: Compiler apne aap change kare(e.g., int to float).
+Explicit: Programmer khud type change kare using (type).
+*/
+
+/* Operator Precedence and Associativity ---------
+
+Operator Precedence:
+It decides which operator is evaluated first when there are multiple operators.
+
+Operator Associativity:
+If same precedence, then associativity decides left to right or right to left evaluation. 
+
+*/
+
+/* Decision Control Statements -----------
+These are used to make decisions in the program.
+
+Statement	                            Description	                                            Example
+if	                                Checks one condition	                                    if (a > b)
+if-else	                            Two-way decision	                                        if (a > b) else
+else-if ladder	                    Multiple conditions	                                        if ... else if ... else
+nested if	                        if inside another if	                                    if (a > 0) { if (a < 10) }
+switch	                            Many fixed cases	                                        switch (choice) { case 1: ... }
+
+*/
+
