a835d4d95bee84b713f3359d2d4c0b66bc9fc245
Isha Ray <isharay009@gmail.com>
2025-08-04 23:37:44 +0530

    string

---
+++ 10/num2.c
@@ -0,0 +1,32 @@
+// Write a C program to find the length of a string using a pointer (without using strlen()).
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *ptr;
+    int length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+
+    ptr = str;
+
+    while (*ptr != '\0') {
+        length++;
+        ptr++;
+    }
+
+    if (str[length - 1] == '\n') {
+        length--;  
+    }
+
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
+
+/* Output :
+Enter a string: isha
+Length of the string: 4
+*/
